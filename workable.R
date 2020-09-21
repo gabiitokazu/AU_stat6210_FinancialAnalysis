8ff3672348e8faef90abca8c834e34e8da4212fc
bijoychandraAU <tzs0075@auburn.edu>
2020-09-21 15:22:41 -0500

    updated get data

--- workable.R
+++ workable.R
@@ -10,5 +10,13 @@ SP500 <- SP500[[1]]
 Tix <- SP500$Symbol
 
 set.seed(10)
-stock=sample(Tix, 5)
-stock
+stocks_tickers=sample(Tix, 5)
+stocks_tickers
+
+three_year_ago <- seq(as.Date("2020-04-01"), length = 2, by = "-3 year")[2]
+getSymbols(stocks_tickers, from = three_year_ago, to = as.Date("2020-04-01"))
+
+stock1<- na.omit(ClCl(get(stocks_tickers[1])))
+stock2 <- na.omit(ClCl(get(stocks_tickers[2])))
+sigma_stocks <- var(cbind(stock1, stock2))
+
